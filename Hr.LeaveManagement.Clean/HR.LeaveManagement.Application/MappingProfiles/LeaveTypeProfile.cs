af5eb50fca31b64b05ccb0ad63cdec8d09fe7d0e
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-06-27 13:42:39 +0300

    Add ServiceCollection Registration and Load-up MappingProfiles

---
+++ Hr.LeaveManagement.Clean/HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfiles
+{
+    public class LeaveTypeProfile : Profile
+    {
+        public LeaveTypeProfile()
+        {
+            //CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+            //from LeaveTypeDto to LeaveType and from LeaveType to LeaveTypeDto with this .ReverseMap()
+        }
+    }
+}
