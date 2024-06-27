a6a23dcb1d0520a721a5ecb32edbedc75f6a03d5
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-06-27 16:59:47 +0300

    Add domain entities configuration

--- Hr.LeaveManagement.Clean/HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
+++ Hr.LeaveManagement.Clean/HR.LeaveManagement.Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Domain.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace HR.LeaveManagement.Persistence.DatabaseContext
@@ -13,5 +14,29 @@ namespace HR.LeaveManagement.Persistence.DatabaseContext
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //apply every configuration that you find in this assembly
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrDatabaseContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
+            {
+                entry.Entity.DateModified = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
