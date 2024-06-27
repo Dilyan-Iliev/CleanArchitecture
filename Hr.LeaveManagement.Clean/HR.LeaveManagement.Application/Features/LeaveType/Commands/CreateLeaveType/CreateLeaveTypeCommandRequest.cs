2609e9b9b474cc2dfed5fb1006f1fd8303960d34
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-06-27 15:35:20 +0300

    CQRS over LeaveType - Part 3

---
+++ Hr.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandRequest
+        : IRequest<int> //will return the id of the created LeaveType
+    {
+        //what we need to create LeaveType:
+        public string Name { get; set; } = string.Empty;
+
+        public int DefaultDays { get; set; }
+    }
+}
---
+++ Hr.LeaveManagement.Clean/HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandRequestHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType
+{
+    public class CreateLeaveTypeCommandRequestHandler
+        : IRequestHandler<CreateLeaveTypeCommandRequest, int>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveTypeCommandRequestHandler(IMapper mapper,
+            ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<int> Handle(CreateLeaveTypeCommandRequest request,
+            CancellationToken cancellationToken)
+        {
+            //Validate incoming data - TODO
+
+            //Convert to domain entity
+            var leaveType = _mapper.Map<Domain.LeaveType>(request);
+
+            //Perform DB operation
+            await _leaveTypeRepository.CreateAsync(leaveType);
+
+            //Return result
+            return leaveType.Id;
+        }
+    }
+}
