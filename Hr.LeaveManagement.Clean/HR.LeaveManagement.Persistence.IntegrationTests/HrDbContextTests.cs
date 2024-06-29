b998140aaa3e7f728d6ced98a40b4c9844da9c3f
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-06-29 10:47:16 +0300

    Integration tests project

---
+++ Hr.LeaveManagement.Clean/HR.LeaveManagement.Persistence.IntegrationTests/HrDbContextTests.cs
@@ -0,0 +1,57 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace HR.LeaveManagement.Persistence.IntegrationTests
+{
+    public class HrDbContextTests
+    {
+        private readonly HrDatabaseContext _db;
+
+        public HrDbContextTests()
+        {
+            //setup mock DB (in-memory DB)
+            var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+                .UseInMemoryDatabase("Test-HrDB").Options;
+
+            _db = new HrDatabaseContext(dbOptions);
+        }
+
+        [Fact]
+        public void Db_Should_Be_Initialized()
+        {
+            _db.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task SaveChanges_Should_SetDateCreatedValue()
+        {
+            //Arrange
+            var leaveType = new LeaveType { Id = 1, DefaultDays = 10, Name = "Test" };
+
+            //Act
+            await _db.LeaveTypes.AddAsync(leaveType);
+            await _db.SaveChangesAsync();
+
+            //Assert
+            _db.LeaveTypes.ShouldNotBeEmpty();
+            leaveType.DateCreated.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task SaveChanges_Should_SetDateModifiedValue()
+        {
+            //Arrange
+            var leaveType = new LeaveType { Id = 1, DefaultDays = 10, Name = "Test" };
+
+            //Act
+            await _db.LeaveTypes.AddAsync(leaveType);
+            await _db.SaveChangesAsync();
+
+            //Assert
+            _db.LeaveTypes.ShouldNotBeEmpty();
+            leaveType.DateModified.ShouldNotBeNull();
+        }
+    }
+}
